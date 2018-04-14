d1134d3260d52832bb9e60325923e3d703b31746
Xavier Schott <assembla@thegothicparty.com>
2018-04-13 17:10:51 -0700

    Introducing UniversalBeacon.Library.iOS

---
+++ Library/UniversalBeacon.Library.iOS/IOSBluetoothPacketProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Foundation;
+using UniversalBeacon.Library.Core.Interfaces;
+using UniversalBeacon.Library.Core.Interop;
+
+namespace UniversalBeacon.Library
+{
+    public class IOSBluetoothPacketProvider : NSObject, IBluetoothPacketProvider
+    {
+        public event EventHandler<BLEAdvertisementPacketArgs> AdvertisementPacketReceived;
+        public event EventHandler<BTError> WatcherStopped;
+
+        public IOSBluetoothPacketProvider()
+        {
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
